8ba4f618f6a28bf460e4d40ab7a35606fd2c8ed2
Terrell-Yan <email@example.com936932230@qq.comgit config --global user.email email@example.com>
2020-08-11 09:02:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/forkotiln/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.forkotiln
+
+import android.database.Observable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity() {
+    var arr_string:MutableList<String> = ArrayList()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button_on.setOnClickListener {
+            Toast.makeText(this, "nihao", Toast.LENGTH_LONG).show()
+        }
+        main()
+      main_for()
+
+    }
+    fun main_for(){
+        val cakes= listOf("var","cage","age")
+        for (cake in cakes){
+            println("ymy,it's  +"+cake)
+        }
+
+    }
+
+    public class StringUtils() {
+        companion object {
+            fun myFun() {
+
+            }
+        }
+    }
+    fun main() {
+        cases("hello")
+    }
+
+    fun cases(obj: Any) {
+        when (obj) {
+            1 -> println("one")
+            "" -> println("Greeting")
+            is Long -> println("Long")
+            !is String -> println("Not a String")
+            else -> println("Unknown")
+        }
+    }
+
+}
